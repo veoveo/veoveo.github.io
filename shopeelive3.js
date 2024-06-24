3483646ae47563c0355c01a64e82b690860c15d4
sjhfdjsh <84219414+veoveo@users.noreply.github.com>
2024-06-24 15:48:21 +0700

    Update and rename shopeelive2.js to shopeelive3.js

--- shopeelive3.js
+++ shopeelive3.js
@@ -9,7 +9,7 @@ document.getElementsByClassName("app-wrapper")[0].innerHTML = `<div class="icon-
 <button id="plus">+</button></div>
 <button style="height:28px;background-color:#ee4d2d;border-radius:4px;font-weight:600;font-size:13px;color:#ffffff;border:0px" class="Button__StyledButton-d958no-0 fWGRcm"><div><a id="coin">0</a>
 <a>🪙 - </a><a id="count">00:00</a></div></button>
-<button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+<button onclick="refresh_coin()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
   <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"></path>
   <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"></path>
 </svg></button>`;
@@ -98,6 +98,32 @@ function lock_coin(coin_id, ss_id) {
       }
     };
     xhr.send(data);
+};
+
+function refresh_coin(ss_id) {
+    var xhr = new XMLHttpRequest();
+    var url = `https://live.shopee.vn/api/v1/session/${se}/coin/user_config?uid=523499622`;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          // Chuyển đổi dữ liệu JSON thành đối tượng JavaScript
+          var responseData = JSON.parse(xhr.responseText);
+          console.log(responseData)
+          if (responseData.data.has_another_device) {
+              console.log("lỗi do có thiết bị khác")
+          } else {
+                if (responseData.data.coins_per_claim>=coin && responseData.data.remain_locks>1 && claim_status == 0) {
+                        lock_coin(responseData.data.coin_id, se);
+                  } 
+          }
+        } else {
+          console.log("(can_claim) Yêu cầu GET không thành công. Mã trạng thái:", xhr.status);
+        }
+      }
+    };
+    xhr.send();
 };
 
 function check_fail_coin(coin_id, ss_id) {
