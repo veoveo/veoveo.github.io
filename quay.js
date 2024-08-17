cba48552a0702ab57f652ad1890a341dac5ce879
sjhfdjsh <84219414+veoveo@users.noreply.github.com>
2024-08-17 09:23:01 +0700

    Update quay.js

--- quay.js
+++ quay.js
@@ -1,3 +1,8 @@
+try {
+    document.cookie.split("SPC_U=")[1].split(";")[0]
+} catch (error) {
+    window.location="https://shopee.vn/buyer/login?next=https://live.shopee.vn/share";
+};
 let list_vq = [];
 document.getElementsByClassName("share__PageWrapper-wtg3fv-3")[0].innerHTML = `<div><div style="height:48px"><div class="app-wrapper" style="max-width: 600px;"><div class="icon-style"><div style="margin-right:8px;width:32px;height:32px"><span width="32px" height="32px" class="ResponsiveImage-wkqiq1-0 jxTcAl"></span></div><div id="shopname">Shopee Live</div></div><button style="height:28px;background-color:#ee4d2d;border-radius:4px;font-weight:600;font-size:13px;color:#ffffff;border:0px" class="Button__StyledButton-d958no-0 fWGRcm"><div><a>🎡</a>
 <a id="coin">0</a>
