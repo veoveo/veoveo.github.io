fc021c5452782712e92aeb116de72425ff028a13
sjhfdjsh <84219414+veoveo@users.noreply.github.com>
2025-12-25 09:50:21 +0700

    Create websocket.js

---
+++ websocket.js
@@ -0,0 +1,45 @@
+let clientId = null;
+
+const socket = new WebSocket("wss://results-amd-common-wholesale.trycloudflare.com/ws");
+
+socket.onmessage = async (event) => {
+  const data = JSON.parse(event.data);
+
+  if (data.type === "init") {
+    clientId = data.id;
+    console.log("Máy A được gán ID:", clientId);
+    return;
+  }
+
+  if (data.cmd === "run") {
+    try {
+      const result = await transformTextHandler(data.args);
+      socket.send(JSON.stringify({
+        clientId: clientId,
+        id: data.id,
+        result: result
+      }));
+    } catch (err) {
+      socket.send(JSON.stringify({
+        clientId: clientId,
+        id: data.id,
+        error: err.toString()
+      }));
+    }
+  }
+};
+
+function transformTextHandler(inputText) {
+  return new Promise((resolve, reject) => {
+    window.bridgeCallHandler("transformText", { text: inputText, method: 3 }, function(response) {
+      var data = response.data;
+      var errorMessage = response.errorMessage;
+      var result = data ? data.result : undefined;
+      if (result) {
+        resolve(result);
+      } else {
+        reject(errorMessage || "Unknown error");
+      }
+    });
+  });
+}
