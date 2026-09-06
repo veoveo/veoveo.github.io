b654856606cd348b393f98f2cff2bea5b6ba4bc5
sjhfdjsh <84219414+veoveo@users.noreply.github.com>
2026-09-06 21:46:28 +0700

    Update gameScene.fa0a00a1ca02d7e4717d.js

--- gameScene.fa0a00a1ca02d7e4717d.js
+++ gameScene.fa0a00a1ca02d7e4717d.js
@@ -1458,7 +1458,7 @@ let bssDFP =
                     }
                     ).start(),
                     this.text.active = !1,
-                    this.db.node.active = !0
+                    this.db.node.active = !0)
                 })
             }
         }
